77b3738fa964b9d569a5186c4e58b1b0a07a0f33
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-11-06 00:53:38 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.AspNetCore.SignalR.Client;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using TestPushNotification.Services;
+
+namespace TestPushNotification
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            HubConnection connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44303/push")
+                .Build();
+            SignalRChatService service = new(connection);
+            service.MessageReceived += Service_MessageReceived;
+            service.Connect().ContinueWith(task =>
+            {
+                if (task.Exception != null)
+                {
+                    MessageBox.Show("Unable to connect to color chat hub");
+                }
+            });
+        }
+
+        private void Service_MessageReceived(PushServer.Lib.Models.ServerMessage obj)
+        {
+            MessageBox.Show(JsonSerializer.Serialize(obj));
+        }
+    }
+}
---
+++ Services/SignalRChatService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+using PushServer.Lib.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPushNotification.Services
+{
+    public class SignalRChatService
+    {
+        private readonly HubConnection _connection;
+
+        public event Action<ServerMessage> MessageReceived;
+
+        public SignalRChatService(HubConnection connection)
+        {
+            _connection = connection;
+
+            _connection.On<ServerMessage>("615eec2da5a1290001c9686b", (message) => MessageReceived?.Invoke(message));
+        }
+
+        public async Task Connect()
+        {
+            await _connection.StartAsync();
+        }
+    }
+}
